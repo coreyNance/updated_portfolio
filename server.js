42d363b5d1f17bf6f3eef1a6bd48e0b0c3690bf5
Corey Nance <coreynance83@gmail.com>
2021-12-15 16:45:59 -0500

    adding server functionality

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
+const path = require('path')
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// app.use(routes);
+// app.get('/', (req, res) => {
 
+//     res.sendFile(path.join(__dirname,'./public/index.html'));
+
+// });
+c
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
